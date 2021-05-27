58a8af2274bc02cc54ffffe46581010c035be729
Hp <Hp@10.10.209.180>
2021-05-27 14:40:49 +0300

    lol

--- 3ahw_financial_application/src/API_tests/test_calls.java
+++ 3ahw_financial_application/src/API_tests/test_calls.java
@@ -1,9 +1,25 @@
 package API_tests;
 
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
 public class test_calls {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		Gson gson = new Gson();
+		URL url = new URL("https://api.tiingo.com/tiingo/daily/<ticker>/prices");
+		InputStreamReader reader = new InputStreamReader(url.openStream());
+		JsonObject o = gson.fromJson(reader, JsonElement.class).getAsJsonObject();
+		System.out.println(o.toString());
+		reader.close();
+
+
 		
-		System.out.println("testcall mafaggaaaaaaaa");
+
 	}
 }
---
+++ 3ahw_financial_application/src/Applciation_interface/MainPage.java
@@ -0,0 +1,14 @@
+package Applciation_interface;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainPage extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+	}
+
+		
+	
+}
