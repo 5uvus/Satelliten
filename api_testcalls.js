c71039f4e2bf1b69f2275f72c42aab79fd302ed3
5uvus <fastrasser@tsn.at>
2021-06-02 16:15:54 +0300

    test

---
+++ api_testcalls.js
@@ -0,0 +1,10 @@
+
+const satellite_id = 1;
+const url = `https://api.spectator.earth/satellite/${satellite_id}`;
+fetch(url)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
