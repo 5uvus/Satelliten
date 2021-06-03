9cfbe7a30fc86c2b33392b5a8640a152245f2e56
5justju5 <1misternoname@gmail.com>
2021-06-04 00:03:55 +0200

    Verzweifelte 2-3 Stunden ohner Erfolg

--- api_testcalls.js
+++ api_testcalls.js
@@ -1,6 +1,8 @@
+const api_key = '7dc22efcaa6595c58a6b1e6e60bc347c0faeae0fa49992e20a9d20340fce1bb8';
 
 const satellite_id = 1;
 const url = `https://api.spectator.earth/satellite/${satellite_id}`;
+
 fetch(url)
   .then(function (response) {
     return response.json();
