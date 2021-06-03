525b8be8f766fee591c38730ac03fcf6b8ae096e
5justju5 <1misternoname@gmail.com>
2021-06-04 00:49:37 +0200

    Funktionier(Allerdings nur mit Chrome Plugin)

--- api_testcalls.js
+++ api_testcalls.js
@@ -1,12 +1,19 @@
 const api_key = '7dc22efcaa6595c58a6b1e6e60bc347c0faeae0fa49992e20a9d20340fce1bb8';
-
-const satellite_id = 1;
-const url = `https://api.spectator.earth/satellite/${satellite_id}`;
-
-fetch(url)
-  .then(function (response) {
+const bbox = '19.59,49.90,20.33,50.21';
+const satellites = 'Sentinel-2A,Sentinel-2B';
+const url = `https://api.spectator.earth/overpass/?api_key=${api_key}&bbox=${bbox}&satellites=${satellites}`;
+  fetch(url).then(function(response) {
     return response.json();
-  })
-  .then(function (data) {
+    }).then(function(data) {
     console.log(data);
-  });
+    });
+    const satellite_id = 1;
+    const url2 = `https://api.spectator.earth/satellite/${satellite_id}`;
+    
+    fetch(url2)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      });
