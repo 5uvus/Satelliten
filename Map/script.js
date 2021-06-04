ee2c06e699336edee839f4ccb558322e05a8f5e1
5uvus <fastrasser@tsn.at>
2021-06-04 20:23:09 +0200

    test

--- Map/script.js
+++ Map/script.js
@@ -1,16 +1,34 @@
-require(["esri/config","esri/Map", "esri/views/MapView"], function (esriConfig,Map, MapView) {
+require([
+  "esri/config",
+  "esri/Map",
+  "esri/views/MapView",
 
-    esriConfig.apiKey = "YOUR_API_KEY";
+  "esri/layers/FeatureLayer"
 
-    const map = new Map({
-      basemap: "arcgis-topographic" // Basemap layer service
-    });
+], function(esriConfig,Map, MapView, FeatureLayer) {
 
-    const view = new MapView({
-      map: map,
-      center: [-118.805, 34.027], // Longitude, latitude
-      zoom: 13, // Zoom level
-      container: "viewDiv" // Div element
-    });
+esriConfig.apiKey = "YOUR_API_KEY";
 
-  });
+const map = new Map({
+  basemap: "arcgis-topographic"
+});
+
+const view = new MapView({
+  container: "viewDiv",
+  map: map,
+  center: [-118.80543,34.02700],
+  zoom: 13
+});
+
+//Trailheads feature layer (points)
+const trailheadsLayer = new FeatureLayer({
+  url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads_Styled/FeatureServer/0"
+});
+//Trails feature layer (lines)
+const trailsLayer = new FeatureLayer({
+  url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trails_Styled/FeatureServer/0"
+});
+
+map.add(trailheadsLayer);
+map.add(trailsLayer, 0);
+});
