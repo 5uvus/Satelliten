5eea348b5060c5f45ef722177f5d8cfae54dde2a
5uvus <fastrasser@tsn.at>
2021-06-04 22:06:26 +0200

    arcgis map test, probleme mit search bar -> keine Ergebnisse

--- Map/script.js
+++ Map/script.js
@@ -1,34 +1,51 @@
+
 require([
   "esri/config",
   "esri/Map",
   "esri/views/MapView",
-
+  "esri/widgets/Search",
   "esri/layers/FeatureLayer"
 
-], function(esriConfig,Map, MapView, FeatureLayer) {
+], function(esriConfig,Map, MapView, Search, FeatureLayer) {
 
 esriConfig.apiKey = "YOUR_API_KEY";
 
 const map = new Map({
-  basemap: "arcgis-topographic"
+  basemap: "dark-gray"
 });
 
 const view = new MapView({
   container: "viewDiv",
   map: map,
   center: [-118.80543,34.02700],
-  zoom: 13
+  zoom: 5
+});
+var searchWidget = new Search({
+  view: view,
+  sources: [{
+    featureLayer: {
+      url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/CongressionalDistricts/FeatureServer/0",
+    },
+    searchFields: ["DISTRICTID"],
+    displayField: "DISTRICTID",
+    exactMatch: false,
+    outFields: ["DISTRICTID", "NAME", "PARTY"],
+    name: "Congressional Districts",
+    placeholder: "example: 3708",
+  }, {
+   featureLayer: {
+      url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/US_Senators/FeatureServer/0",
+  },
+    searchFields: ["Name", "Party"],
+    suggestionTemplate: "{Name}, Party: {Party}",
+    exactMatch: false,
+    outFields: ["*"],
+    name: "Senators",
+  }]
+});
+view.ui.add(searchWidget, {
+  position: "top-right",
+  index: 2
 });
 
-//Trailheads feature layer (points)
-const trailheadsLayer = new FeatureLayer({
-  url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads_Styled/FeatureServer/0"
-});
-//Trails feature layer (lines)
-const trailsLayer = new FeatureLayer({
-  url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trails_Styled/FeatureServer/0"
-});
-
-map.add(trailheadsLayer);
-map.add(trailsLayer, 0);
 });
