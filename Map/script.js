5e65f7379cc48aaac52e5447501bfc78628bf3ae
5justju5 <1misternoname@gmail.com>
2021-06-05 15:04:42 +0200

    hallo

--- Map/script.js
+++ Map/script.js
@@ -8,7 +8,7 @@ require([
 
 ], function(esriConfig,Map, MapView, Search, FeatureLayer) {
 
-esriConfig.apiKey = "YOUR_API_KEY";
+esriConfig.apiKey = "AAPK8f51c7ada42f45f3a312772b619297a0GX-8LymtaW78Crkz_fE6f5Pst5hdHATvlIGf86z0flzOmxp_QAobGzacz_CMP0HT";
 
 const map = new Map({
   basemap: "dark-gray"
@@ -16,9 +16,9 @@ const map = new Map({
 
 const view = new MapView({
   container: "viewDiv",
-  map: map,
-  center: [-118.80543,34.02700],
-  zoom: 5
+ map: map,
+   center: [-118.80543,34.02700],
+  zoom: 3
 });
 var searchWidget = new Search({
   view: view,
@@ -46,6 +46,9 @@ var searchWidget = new Search({
 view.ui.add(searchWidget, {
   position: "top-right",
   index: 2
+});
+const trailheadsLayer = new FeatureLayer({
+  url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads_Styled/FeatureServer/0"
 });
 
 });
