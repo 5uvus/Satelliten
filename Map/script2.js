2972de34f429c08cf765ecba5137242b34383734
5uvus <fastrasser@tsn.at>
2021-06-10 13:56:02 +0300

    screen split for data

--- Map/script2.js
+++ Map/script2.js
@@ -39,4 +39,52 @@ require([
     dom.byId("info").innerHTML = mp.x.toFixed(3) + ", " + mp.y.toFixed(3);
     console.log(mp.x.toFixed(3) + ", " + mp.y.toFixed(3));
   }
+
+
+  const route = {
+    type: "line-3d",
+    symbolLayers: [
+      {
+        type: "line",
+        size: "10px",
+        material: {
+          color: white
+        }
+      },
+      {
+        type: "line",
+        size: "3px",
+        material: {
+          color: blue
+        }
+      }
+    ]
+  };
+  // remove the reference to the container for the previous view
+  appConfig.activeView.container = null;
+  const sketchVM = new SketchViewModel({
+    layer: gLayer,
+    view: view,
+    polygonSymbol: {
+      type: "polygon-3d",
+      symbolLayers: [
+        {
+          type: "extrude",
+          size: 10, // extrude by 10 meters
+          material: {
+            color: white
+          },
+          edges: {
+            type: "solid",
+            size: "3px",
+            color: [82, 82, 122, 0.8]
+          }
+        }
+      ]
+    },
+    defaultCreateOptions: { hasZ: false }
+  });
+  button.addEventListener("click", function (event) {
+    sketchVM.create("polygon");
+  });
 });
