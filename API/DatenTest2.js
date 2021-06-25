76f62da59fd1b9808c5219d389a41f19667fb90b
5uvus <fastrasser@tsn.at>
2021-06-25 10:05:26 +0300

    parsen funkt jetzt auch

--- API/DatenTest2.js
+++ API/DatenTest2.js
@@ -3,9 +3,15 @@ var request = new XMLHttpRequest();
 const satellite_id = 1;
 
 function reqListener () {                     //Gibt die API-Calls aus in der Konsole
-    console.log(this.responseText);
+    //console.log(request.responseText);
   }
   
   request.addEventListener("load", reqListener);
   request.open("GET", `https://api.spectator.earth/satellite/${satellite_id}`);
+  request.onload  = function() {
+    var jsonResponse = JSON.parse(request.responseText);;
+    console.log(jsonResponse.geometry);
+    // do something with jsonResponse
+ };
   request.send();
+ 
